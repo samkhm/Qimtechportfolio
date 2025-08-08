84f5aa429fe193360202aa1b55f96d8649e9ebc4
Samuel Kimanthi <samuelkimanthi02@gmail.com>
2025-08-08 13:12:24 +0300

    added projects

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -108,7 +108,7 @@ export default function Projects() {
   };
 
   return (
-    <div className="max-w-screen p-5 mr-5 ml-5 bg-gray-300/80 rounded" id="projects">
+    <div className="max-w-screen p-5 mr-5 ml-5 bg-gray-100/80 rounded" id="projects">
       <section
         aria-labelledby="projects-heading"
         className="container py-12 font-sans antialiased"
