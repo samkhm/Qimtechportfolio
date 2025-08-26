ec2fcf25b8018e7e6ee9505aa6faacadd6093406
Samuel Kimanthi <samuelkimanthi02@gmail.com>
2025-08-26 18:07:22 +0300

    Load services with no restrictions

--- server/routes/adminTasksRoutes.js
+++ server/routes/adminTasksRoutes.js
@@ -8,27 +8,27 @@ const { createTestimony, getAllTestimonies, deleteTestimonies, approveTestimony}
 const router = express.Router();
 
 router.post("/hobby", protect, authorize(["admin"]), createHobby );
-router.get("/hobby", protect, authorize(["admin"]), getAllHobbies);
+router.get("/hobby", getAllHobbies);
 router.put("/hobby/:id", protect, authorize(["admin"]), updateHobby);
 router.delete("/hobby/:id", protect, authorize(["admin"]), deleteHobby);
 
 router.post("/project", protect, authorize(["admin"]), createProject);
-router.get("/project", protect, authorize(["admin"]), getAllProjects);
+router.get("/project", getAllProjects);
 router.put("/project/:id", protect, authorize(["admin"]), updateProject);
 router.delete("/project/:id", protect, authorize(["admin"]), deleteProject);
 router.put("/project_complete/:id", protect, authorize(["admin"]), toggleCompleted);
 
 router.post("/services", protect, authorize(["admin"]), createService);
-router.get("/services", protect, authorize(["admin"]), getAllServices);
+router.get("/services", getAllServices);
 router.put("/services/:id", protect, authorize(["admin"]), updateService);
 router.delete("/services/:id", protect, authorize(["admin"]), deleteService);
 
 router.post("/skills", protect, authorize(["admin"]), createSkill);
-router.get("/skills", protect, authorize(["admin"]), getAllSkills);
+router.get("/skills", getAllSkills);
 router.put("/skills/:id", protect, authorize(["admin"]), updateSkill);
 router.delete("/skills/:id", protect, authorize(["admin"]), deleteSkill);
 
-router.get("/testimony", protect, authorize(["admin"]), getAllTestimonies);
+router.get("/testimony", getAllTestimonies);
 router.delete("/testimony/:id", protect, authorize(["admin"]), deleteTestimonies);
 router.put("/testimony/:id", protect, authorize(["admin"]), approveTestimony)
 
